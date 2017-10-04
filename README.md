2b1f875d3b5492cbe1ab5efb31a5f8693914cc4a
gregoirepuget <gregoire@36pixels.fr>
2017-10-05 01:46:01 +0200

    first really commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 36 Pixels package
+
+Snippets used in 36 Pixels agency.
+
+![A screenshot of your package](https://f.cloud.github.com/assets/69169/2290250/c35d867a-a017-11e3-86be-cd7c5bf3ff9b.gif)
---
+++ spec/36-pixels-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import 36PixelsPackageView from '../lib/36-pixels-package-view';
+
+describe('36PixelsPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
