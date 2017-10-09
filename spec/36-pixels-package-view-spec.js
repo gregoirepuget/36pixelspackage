a0fc05c42f5df2865722cb8aa852ab775ff8f30b
gregoirepuget <gregoire@36pixels.fr>
2017-10-09 21:27:18 +0200

    ok

--- spec/36-pixels-package-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import 36PixelsPackageView from '../lib/36-pixels-package-view';
-
-describe('36PixelsPackageView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
